9d4acb6849661689d43e0aaf9cda1411fce0c70a
finbyz <info@finbyz.tech>
2024-10-21 15:02:04 +0530

    removed 4 doctypes with ticket ISS-13773

--- ibtevolve/ibtevolve/doctype/drec_leasing/drec_leasing.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, FinByz Tech and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('DREC Leasing', {
-	refresh: function(frm) {
-
-	}
-});
--- ibtevolve/ibtevolve/doctype/drec_tenant_details/drec_tenant_details.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, FinByz Tech and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('DREC Tenant Details', {
-	refresh: function(frm) {
-
-	}
-});
