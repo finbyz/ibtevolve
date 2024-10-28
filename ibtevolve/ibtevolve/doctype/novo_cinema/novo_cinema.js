efff024b8a745b75d2bb29a1ffdb132a90259728
finbyz <info@finbyz.tech>
2024-10-28 19:39:52 +0530

    Delete Doctypes

--- ibtevolve/ibtevolve/doctype/novo_cinema/novo_cinema.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, FinByz Tech and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Novo Cinema', {
-	refresh: function(frm) {
-
-	}
-});
--- ibtevolve/ibtevolve/doctype/olw/olw.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, FinByz Tech and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('OLW', {
-	refresh: function(frm) {
-
-	}
-});
