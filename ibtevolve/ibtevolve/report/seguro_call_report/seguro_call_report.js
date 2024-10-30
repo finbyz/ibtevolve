59c2fa87ddf1c79722471800bad11ce1bb1877df
finbyz <info@finbyz.tech>
2024-10-30 17:40:23 +0530

    Deleted Docs

--- ibtevolve/ibtevolve/report/seguro_call_report/seguro_call_report.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016, FinByz Tech and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["Seguro Call Report"] = {
-	"filters": [
-
-	]
-}
